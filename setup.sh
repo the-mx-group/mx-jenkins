45d2cdd15d6c17e6ca69fc87bae31f8ca18c606d
eric vz <evonzee@themxgroup.com>
2017-08-17 18:00:44 -0500

    Get docker-ce directly from docker since they removed it from debian's repos

--- setup.sh
+++ setup.sh
@@ -6,7 +6,23 @@ apt-get update
 apt-get install -y nodejs
 
 # install docker so we can call parentdocker via socket
-apt-get install -y docker.io
+# now that docker is docker-ce, debian dropped it, so this is complicated
+# from https://docs.docker.com/engine/installation/linux/docker-ce/debian/#install-using-the-repository
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg > /tmp/docker-gpg-key
+gpg --no-default-keyring --keyring /tmp/docker.gpg /tmp/docker-gpg-key | grep "9DC858229FC7DD38854AE2D88D81803C0EBFCD88" || exit
+cat /tmp/docker-gpg-key | apt-key add -
+add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) \
+    stable"
+apt-get update
+apt-get install -y docker-ce
 
 #install awscli with pip to get the latest
 apt-get install -y python-pip
