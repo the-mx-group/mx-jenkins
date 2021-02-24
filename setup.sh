de29940a72d3252b3c2cea4951f821b1e36d610f
eric vz <evonzee@themxgroup.com>
2021-02-24 17:45:11 -0600

    don't try to use sudo in setup

--- setup.sh
+++ setup.sh
@@ -46,10 +46,10 @@ apt-get install -y --allow-unauthenticated --no-install-recommends git-lfs
 git lfs install
 
 echo "Installing kubernetes tools..."
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
 
 echo "Cleaning up..."
 apt-get clean
