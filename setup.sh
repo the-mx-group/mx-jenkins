73b8b3fc63369ef93890affe2895a229fd928817
eric vz <evonzee@themxgroup.com>
2022-05-05 15:44:56 -0500

    force kubectl version 1.23.6

--- setup.sh
+++ setup.sh
@@ -49,7 +49,7 @@ echo "Installing kubernetes tools..."
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
-apt-get install -y kubectl
+apt-get install -y kubectl=1.23.6-00
 
 echo "Installing rsync..."
 apt-get install -y rsync
